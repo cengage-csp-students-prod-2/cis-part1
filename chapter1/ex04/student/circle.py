49590b61f783b07ee8399c05caf5f0120934c2da
k1ng <jahshuagrant7@gmail.com>
2025-01-24 03:03:44 +0000

    Testing Fri Jan 24 2025 03:03:44 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04/student/circle.py
+++ chapter1/ex04/student/circle.py
@@ -1,5 +1,4 @@
 # Write your code here
 radius = float(input("Enter radius: "))
-
-area = 3.14 * radius **2
+area = 3.14 * radius ** 2
 print("The area is", area, "square units.")
--- chapter1/ex05/student/cuboid.py
+++ chapter1/ex05/student/cuboid.py
@@ -1,1 +1,6 @@
 # Write your program here
+width = float(input("Enter width length: "))
+height = float(input("Enter the height: "))
+depth = float(input("Enter the depth: "))
+volume = depth * width * height 
+print("The volume is", volume, "cubic units.")
