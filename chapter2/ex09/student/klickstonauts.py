682bd9dea124c5ce7027a5ac411613c5b8ced94c
k1ng <jahshuagrant7@gmail.com>
2025-01-30 20:55:57 +0000

    Testing Thu Jan 30 2025 20:55:57 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex09/student/klickstonauts.py
+++ chapter2/ex09/student/klickstonauts.py
@@ -2,7 +2,7 @@
 KM_TO_NAUTICAL_MILE_CONVERSION = 1 / (1/10000 * 90 * 60)
 
 # Prompt the user to enter the distance in kilometers
-kilometers = float(input("Enter the distance in kilometers: "))
+kilometers = int(input("Enter the distance in kilometers: "))
 
 # Calculate the equivalent distance in nautical miles
 nautical_miles = kilometers * KM_TO_NAUTICAL_MILE_CONVERSION
