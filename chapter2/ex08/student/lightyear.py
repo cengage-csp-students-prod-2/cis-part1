cf97090f880a4a37732aff3f349663959be90c1d
k1ng <jahshuagrant7@gmail.com>
2025-01-30 20:47:32 +0000

    Testing Thu Jan 30 2025 20:47:32 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex08/student/lightyear.py
+++ chapter2/ex08/student/lightyear.py
@@ -3,7 +3,7 @@ SECONDS_PER_YEAR = 365 * 24 * 60 * 60
 SPEED_OF_LIGHT = 3 * 10**8  
 num_of_years = float(input("Enter number of years: "))
 
-distance = years * SPEED_OF_LIGHT * SECONDS_PER_YEAR
+distance = num_of_years * SPEED_OF_LIGHT * SECONDS_PER_YEAR
 
 print(f"The distance light travels in {num_of_years} years is {distance} meters")
 
