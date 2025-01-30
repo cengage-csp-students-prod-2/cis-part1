2483e81d7fadeeaf1d86f3446059346a4e5a28d2
k1ng <jahshuagrant7@gmail.com>
2025-01-30 20:48:09 +0000

    Testing Thu Jan 30 2025 20:48:09 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex08/student/lightyear.py
+++ chapter2/ex08/student/lightyear.py
@@ -1,7 +1,7 @@
 # Write your program here
 SECONDS_PER_YEAR = 365 * 24 * 60 * 60  
 SPEED_OF_LIGHT = 3 * 10**8  
-num_of_years = float(input("Enter number of years: "))
+num_of_years = int(input("Enter number of years: "))
 
 distance = num_of_years * SPEED_OF_LIGHT * SECONDS_PER_YEAR
 
