a547fde5f818ce22aedbc95b769ad39a19a8bee5
k1ng <jahshuagrant7@gmail.com>
2025-01-30 19:48:37 +0000

    Testing Thu Jan 30 2025 19:48:36 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/cube.py
+++ chapter2/ex02/student/cube.py
@@ -6,4 +6,4 @@
 
 edge_length = int(input("Enter edge length: "))
 surface_area = 6 * (edge_length ** 2)
-print(f"The surface area of the cube is {surface_area}")
+print(f"The surface area of the cube is {surface_area} square units.")
