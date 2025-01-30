1e62b1d333c825fe8d5769ee690635b1e0fcb4d2
k1ng <jahshuagrant7@gmail.com>
2025-01-30 20:09:10 +0000

    Testing Thu Jan 30 2025 20:09:10 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex05/student/momentum.py
+++ chapter2/ex05/student/momentum.py
@@ -1,1 +1,21 @@
-# Write your program here
+"""
+Pseudocode:
+1. Start
+2. Prompt user for the mass
+3. Prompt user for the velocity
+4. Calculate momentum as mass * velocity
+5. Display momentum
+6. End
+"""
+
+# Prompt the user to enter the mass of the object
+mass = float(input("Enter the mass of the object (in kilograms): "))
+
+# Prompt the user to enter the velocity of the object
+velocity = float(input("Enter the velocity of the object (in meters per second): "))
+
+# Calculate the momentum
+momentum = mass * velocity
+
+# Display the calculated momentum
+print(f"The momentum of the object is {momentum} kg·m/s")
