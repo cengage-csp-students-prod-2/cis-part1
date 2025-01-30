0ebdd9b1045bc7b48e8300107ca0a637a51e56f4
k1ng <jahshuagrant7@gmail.com>
2025-01-30 20:17:31 +0000

    Testing Thu Jan 30 2025 20:17:31 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/momentum.py
+++ chapter2/ex06/student/momentum.py
@@ -1,1 +1,10 @@
-# Write your program here
+mass = float(input("Enter the mass of the object (in kilograms): "))
+
+velocity = float(input("Enter the velocity of the object (in meters per second): "))
+
+momentum = mass * velocity
+
+kinetic_energy = 0.5 * mass * (velocity ** 2)
+
+print(f"The momentum of the object is {momentum} kg·m/s")
+print(f"The kinetic energy of the object is {kinetic_energy} joules")
