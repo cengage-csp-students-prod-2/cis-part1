4abeaa0ea4cd153fe8e3ceaa3cc35bbe8b174b6b
k1ng <jahshuagrant7@gmail.com>
2025-01-30 19:57:22 +0000

    Testing Thu Jan 30 2025 19:57:22 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/fivestar.py
+++ chapter2/ex03/student/fivestar.py
@@ -1,1 +1,9 @@
-# Write your program here
+new_video = 3.00
+old_video = 2.00
+
+num_new_videos = int(input("Enter the number of new videos: "))
+num_old_videos = int(input("Enter the number of old videos: "))
+
+total_cost = (num_new_videos * new_video) + (num_old_videos * old_video)
+
+print(f"The total cost is ${total_cost:.2f}")
