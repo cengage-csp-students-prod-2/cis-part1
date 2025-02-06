f4bdfd0f2041407674776ac3b83f82519a5d3d48
k1ng <jahshuagrant7@gmail.com>
2025-02-06 17:50:56 +0000

    Testing Thu Feb 06 2025 17:50:56 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex06/student/leibniz.py
+++ chapter3/ex06/student/leibniz.py
@@ -1,1 +1,16 @@
-# Write your program here
+# Prompt the user to enter the number of iterations
+iterations = int(input("Enter the number of iterations: "))
+
+# Initialize variables
+approximation = 0.0
+
+# Calculate the approximation of π using the Leibniz formula
+for i in range(iterations):
+    term = (-1) ** i / (2 * i + 1)
+    approximation += term
+
+# Multiply by 4 to get the approximation of π
+pi_approximation = 4 * approximation
+
+# Display the resulting value of π
+print(f"The approximation of π after {iterations} iterations is {pi_approximation}")
