c3ccfde7bf2ea2fca5cefab1b63373c7b16e2d58
k1ng <jahshuagrant7@gmail.com>
2025-02-06 16:51:51 +0000

    Testing Thu Feb 06 2025 16:51:51 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex02/student/right.py
+++ chapter3/ex02/student/right.py
@@ -1,2 +1,9 @@
-# Write your program here
-print('Hello')
+side1 = int(input("Enter the first side: "))
+side2 = int(input("Enter the second side: "))
+side3 = int(input("Enter the third side: "))
+
+if side1**2 + side2**2 == side3**2:
+    print("The triangle is a right triangle.")
+
+else:
+    print("The triangle is not a right triangle.")
