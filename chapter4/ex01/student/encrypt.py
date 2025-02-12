2b1d41750c266b372b30875be86be38168b63850
k1ng <jahshuagrant7@gmail.com>
2025-02-12 20:26:36 +0000

    Testing Wed Feb 12 2025 20:26:36 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/encrypt.py
+++ chapter4/ex01/student/encrypt.py
@@ -1,34 +1,17 @@
-def caesar_cipher(plaintext, distance):
-    encrypted_text = []
-    
-    for char in plaintext:
-        # Check if the character is printable
+def caesar_cipher(text, distance):
+    encrypted_text = ""
+    for char in text:
         if char.isprintable():
-            # Shift the character by the distance
-            shifted_char = chr((ord(char) + distance) % 256)
-            encrypted_text.append(shifted_char)
+            encrypted_char = chr((ord(char) + distance) % 256)
+            encrypted_text += encrypted_char
         else:
-            # If not printable, keep the character unchanged
-            encrypted_text.append(char)
-    
-    return ''.join(encrypted_text)
+            encrypted_text += char
+    return encrypted_text
 
 def main():
-    # Input plaintext from the user
     plaintext = input("Enter the plaintext: ")
-    
-    # Input distance value from the user
-    while True:
-        try:
-            distance = int(input("Enter the distance value (integer): "))
-            break
-        except ValueError:
-            print("Please enter a valid integer for the distance value.")
-    
-    # Encrypt the plaintext
+    distance = int(input("Enter the distance value: "))
     encrypted_text = caesar_cipher(plaintext, distance)
-    
-    # Output the encrypted text
     print("Encrypted text:", encrypted_text)
 
 if __name__ == "__main__":
